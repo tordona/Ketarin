bc827eb226464fab80cc180e6d862f0197f6d553
Terence Ordona <terence.ordona@gmail.com>
2026-03-16 12:38:01 -0400

    build: adjust assembly info

---
+++ dependencies/diffiehellman/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("diffiehellman")]
+[assembly: AssemblyDescription("Diffie-Hellman implementation for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiffieHellman.Properties")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b7634288-162c-4347-b586-8f04bf284d36")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
